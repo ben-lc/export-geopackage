8fe2748135f434e100d210c12e41fd037dadce39
ben-lc <38050807+ben-lc@users.noreply.github.com>
2023-01-30 21:16:23 +0100

    chore: add gradle plugins for formatting, git release and updates checks

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,33 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.7.20"
+  kotlin("jvm") version "1.7.20"
+  id("com.diffplug.spotless") version "6.11.0"
+  id("com.github.ben-manes.versions") version "0.43.0"
+  id("pl.allegro.tech.build.axion-release") version "1.14.2"
 }
 
 group = "fr.benlc"
-version = "1.0-SNAPSHOT"
+
+version = scmVersion.version
 
 repositories {
-    mavenCentral()
+  mavenCentral()
+  maven { url = uri("https://repo.osgeo.org/repository/release/") }
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+  testImplementation(kotlin("test"))
 }
 
-tasks.test {
-    useJUnitPlatform()
-}
+tasks.test { useJUnitPlatform() }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "17"
+tasks.withType<KotlinCompile> { kotlinOptions.jvmTarget = "17" }
+
+spotless {
+  kotlin { ktfmt() }
+  kotlinGradle {
+    target("*.gradle.kts")
+    ktfmt()
+  }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,1 @@
-
 rootProject.name = "export-geopackage"
-
